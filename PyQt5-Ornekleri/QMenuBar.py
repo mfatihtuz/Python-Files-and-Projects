c3930e525d7314df761b6c660cef5c7504e5a6ba
Fatih TÜZ <m.fatihtuz@gmail.com>
2020-05-28 02:54:57 +0300

    Update QMenuBar.py
    PyQt5'te QMenuBar() sınıfı ile Menü Çubuğu oluşturmak

--- PyQt5-Ornekleri/QMenuBar.py
+++ PyQt5-Ornekleri/QMenuBar.py
@@ -15,20 +15,27 @@ class Ornek(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+    
     def initUI(self):
+        
+        #Pencere Özellikleri ayarlandı
         self.setGeometry(200,300,500,200)
         self.setWindowTitle("Basit Bir Menü")
 
+        #QMenuBar sınıfından Menü Çubuğu Objesi Tanımlaması, ve Çubuğa Menü eklenmesi
         menu_cubugu = self.menuBar()
         dosya_menusu = menu_cubugu.addMenu("&Dosya")
-
+        
+        #Menü ile alakalı bilgileri durum çubuğunda göstermek için, durum çubuğu oluşturmak
         self.statusBar()
-
+        
+        #Menü'nün yapacağı işlevin belirlenmesi
         cikis_eylemi = QAction(QIcon('cikis.png'), '&Çıkış', self)
         cikis_eylemi.setShortcut('Ctrl+Ç')
         cikis_eylemi.setStatusTip("Bu menü programdan çıkış yapmanızı sağlar")
         cikis_eylemi.triggered.connect(qApp.quit)
 
+        #Oluşturulan menüye belirlenen eylemin atanması
         dosya_menusu.addAction(cikis_eylemi)
         self.show()
 
