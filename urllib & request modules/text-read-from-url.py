2f638d5522990c5468521b3312789acfa97ab50e
Fatih TÜZ <m.fatihtuz@gmail.com>
2020-05-28 03:16:21 +0300

    Update text-read-from-url.py

--- urllib & request modules/text-read-from-url.py
+++ urllib & request modules/text-read-from-url.py
@@ -1,34 +1,35 @@
 from urllib.request import urlopen
 import urllib.request
 
-while(True):
-    
-    # URL'deki okunacak .txt dosyasına erişme
-    # Accessing the .txt file to be read in the URL
-    url = "https://www.yourwebsite.com/your_text_file.txt"
-    file = urllib.request.urlopen(url)
+# URL'deki okunacak .txt dosyasına erişme
+# Accessing the .txt file to be read in the URL
+url = "https://www.yourwebsite.com/your_text_file.txt"
+file = urllib.request.urlopen(url)
 
-    kullanicilar = [] #users
-    user_kontrol = "your-user-kontrol-text"
-    
-    # url'deki .txt dosyasını oku ve listeye ata
-    # read txt file in url and assing to the list
-    for line in file:
-        kullanicilar.append(str(line.strip(), 'utf-8'))
-        print(kullanicilar)
-    
-    # If the user is finded, then set '1' the flag
-    users_flag = 0 
-    for i in kullanicilar:
-        if i == user_kontrol:
-            users_flag = 1
-            break
-        else:
-            users_flag = 0
+kullanicilar = [] #users
+user_kontrol = "your-user-kontrol-text"
 
-    if (users_flag == 1):
-        print("Tebrikler! Listedesiniz :) ")
+# url'deki .txt dosyasını oku ve listeye ata
+# read txt file in url and assing to the list
+for line in file:
+    kullanicilar.append(str(line.strip(), 'utf-8'))
+    print(kullanicilar)
+
+# If the user is finded, then set '1' the flag
+users_flag = 0 
+for i in kullanicilar:
+    if i == user_kontrol:
+        users_flag = 1
+        break
     else:
-        print("Üzgünüm! Listede değilsiniz :( ")
+        users_flag = 0
+
+if (users_flag == 1):
+    print("Tebrikler! Listedesiniz :) ")
+else:
+    print("Üzgünüm! Listede değilsiniz :( ")
+        
+        
+    
 
 
