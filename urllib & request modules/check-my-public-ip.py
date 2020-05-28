03cf85b1c9a6de2a16a611afe052c7e722a648eb
Fatih TÜZ <m.fatihtuz@gmail.com>
2020-05-28 03:29:28 +0300

    Update check-my-public-ip.py
    Public IP adresi öğrenme. Ve 10 adet farklı IP adresinden programa giriş yapıldığı için uyarı veren program.
    
    Finding public IP address. Also, the program that warns you if the it is logged in from 10 different IP addresses.

--- urllib & request modules/check-my-public-ip.py
+++ urllib & request modules/check-my-public-ip.py
@@ -1,16 +1,25 @@
+# Program that gives warning when entering a program from X different number of IPs.
+# Bir programa X adet farklı sayıda IP'den giriş yapıldığında, uyarı veren program.
+
 from requests import get
+
+#Check your Public IP
+ip_flag = 0
+my_ip = get('https://ipapi.co/ip/').text
 
 check_my_ip_list = []
 
+# Checking the previous saved IP addresses
+# Önceki kaydedilen IP adreslerinin kontrol edilmesi
 f_ip = open("ips.txt", "r", encoding="UTF-8")
 for i in f_ip:
-    check_my_ip_list.append(i.strip())
+    check_my_ip_list.append(i.strip())   
 if len(check_my_ip_list) > 10:
-    print("Çok fazla sayıda IP girişi oldu, lütfen satıcıyla iletişime geçin")
+    # X (burada 10) adet farklı IP adresinden giriş yapıldığı için, program uyarı veriyor.
+    # Since the program is logged in from X (in this case 10) different IP addresses, the program gives a warning.
+    print("Çok fazla sayıda IP girişi oldu, lütfen programcıyla iletişime geçin")
 else:
     print(check_my_ip_list)
-    ip_flag = 0
-    my_ip = get('https://ipapi.co/ip/').text
     for i in check_my_ip_list:
         if my_ip == i:
             ip_flag = 1
@@ -19,16 +28,13 @@ else:
             ip_flag = 0
 
     if ip_flag == 1:
+        # IP Adresi Bulunmakta. İşleme Sorunsuz Şekilde Devam Edilebilir
+        # IP Address has found in the list. Process can continue seamlessly
         print(my_ip + " IP Adresi Bulunmakta. İşleme Sorunsuz Şekilde Devam Edilebilir")
     else:
         # Yeni IP Adresi Dosyaya Eklendi
+        # New IP adress is added to the list
         print(my_ip + " Yeni IP Adresi Ekleniyor")
         f = open("ips.txt", "a+")
         f.write("\n" + my_ip)
         f.close()
-
-cks = input("Kapatmak için y/Y tuşuna basın : ")
-if (cks == 'y' or cks == 'Y'):
-    break
-else:
-    print("\n\nProgram Tekrardan Başlatılıyor\n\n")
